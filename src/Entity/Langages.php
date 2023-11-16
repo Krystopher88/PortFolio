defdc95063275635d0b140a0d9956c1e20071def
Krystopher88 <bichon.christ@gmail.com>
2023-11-16 14:28:33 +0100

    Entity langages

---
+++ src/Entity/Langages.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LangagesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LangagesRepository::class)]
+class Langages
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 32)]
+    private ?string $langage = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $file_name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLangage(): ?string
+    {
+        return $this->langage;
+    }
+
+    public function setLangage(string $langage): static
+    {
+        $this->langage = $langage;
+
+        return $this;
+    }
+
+    public function getFileName(): ?string
+    {
+        return $this->file_name;
+    }
+
+    public function setFileName(?string $file_name): static
+    {
+        $this->file_name = $file_name;
+
+        return $this;
+    }
+}
