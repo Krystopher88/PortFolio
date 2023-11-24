5874df31e4dd26f9367570643989426339266209
Krystopher88 <bichon.christ@gmail.com>
2023-11-24 16:48:52 +0100

    Header

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,6 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
 require('bootstrap');
+require('bootstrap-icons/font/bootstrap-icons.css');
+import './styles/app.scss';
